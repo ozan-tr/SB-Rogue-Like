1d40938e4a684a66ecced010493855a705813f13
ozan-tr <ozang@live.com>
2023-12-16 21:44:45 +0300

    redbull fix

--- assets/js/class/items/weapons/RedBull.js
+++ assets/js/class/items/weapons/RedBull.js
@@ -19,7 +19,8 @@ class RedBull extends WeaponBase {
 
         this.maxLevel = this.stats.length - 1;
 
-        this.sprite = document.querySelector("RedBull").children.sor((a,b)=>{a.id[0]-b.id[0]})
+
+        this.sprites = Array.from(document.getElementById("RedBull").children).sort((a,b)=>{a.id[0]-b.id[0]})
     }
     attack() {
         const animationCanvas = document.createElement("canvas");
@@ -29,24 +30,28 @@ class RedBull extends WeaponBase {
         document.getElementsByClassName("mainContainer")[0].appendChild(animationCanvas);
         const anim = animationCanvas.getContext("2d");
 
-        const playerPos = player.getTruePos();
-
-        const stats = this.stats[this.level];
+        var playerPos = player.getTruePos()
 
         const timeOut = new Date();
         const lifeSpan = 3000 * stats.duration;
 
+
+        const totalSprites = this.sprites.length
+
+        var spriteIndex = 0
+
+        const animSpeed = 1000/(stats.attackSpeed*totalSprites)
+
+        var animTime = new Date()
+
         var angle;
 
         if(allMobs.length > 0){
-            const nearestMob = allMobs.sort((a,b)=>{
-                Math.hypot(playerPos.x - a.pos.x,playerPos - a.pos.y) - Math.hypot(playerPos.x - b.pos.x,playerPos.y - b.pos.y)
-            })[0]
-
-            angle = Math.atan2(playerPos.x-nearestMob.pos.x, playerPos.y-nearestMob.pos.y)
+            var nearestMob = allMobs.sort((a,b)=>{return a.distToPlayer-b.distToPlayer})[0]
+            angle = Math.atan2(playerPos.y-nearestMob.pos.y,playerPos.x-nearestMob.pos.x)
             console.log("mobFound:"+angle)
         }else{
-            angle = Math.atan2((Math.random*2)-1, (Math.random*2)-1)
+            angle = Math.atan2(Math.floor((Math.random()*2)-1), Math.floor(Math.random()*3)-1)
             console.log("noMob:"+angle)
         }
 
@@ -55,24 +60,37 @@ class RedBull extends WeaponBase {
         var howManyHits = 0;
 
         const attackAnimation = setInterval(() => {
-            const relativePos = { x: playerPos.x + player.pos.x, y: playerPos.y + player.pos.y }
+
+            if(new Date()-animTime>animSpeed){
+                spriteIndex++
+                if(spriteIndex>totalSprites-1){spriteIndex=0}
+                animTime = new Date()
+            }
+
+            const stats = this.stats[this.level];
+
+            const size = stats.size * player.getStat("size");
+            const attackSpeed = stats.attackSpeed * player.getStat("attackSpeed");
+
+            const sprite = this.sprites[spriteIndex]
+
+            pos.x -= Math.cos(angle) * attackSpeed * 2;
+            pos.y -= Math.sin(angle) * attackSpeed * 2;
+
+            const x = pos.x - sprite.width * size / 2;
+            const y = pos.y - sprite.height * size / 2;
+
+            const relativePos = {x:playerPos.x+player.pos.x,y:playerPos.y+player.pos.y}
 
             anim.setTransform(1, 0, 0, 1, relativePos.x, relativePos.y);
-
-            pos.x += Math.cos(angle) * stats.attackSpeed * 2;
-            pos.y += Math.sin(angle) * stats.attackSpeed * 2;
-
-            const x = pos.x - sprite.width / 2;
-            const y = pos.y - sprite.height / 2;
-
             anim.clearRect(-relativePos.x, -relativePos.y, c.width, c.height);
 
             anim.save()
 
             anim.translate(x, y)
-            anim.rotate(angle + Math.PI / 2)
+            anim.rotate(angle)
 
-
+            anim.drawImage(sprite, 0, 0, sprite.width * size, sprite.height * size);
 
             anim.restore()
 
@@ -81,8 +99,8 @@ class RedBull extends WeaponBase {
             const hitbox = {
                 x: truePos.x + x,
                 y: truePos.y + y,
-                w: sprite.width,
-                h: sprite.height
+                w: sprite.width * size,
+                h: sprite.height * size
             };
 
             this.checkHit(hitbox);
